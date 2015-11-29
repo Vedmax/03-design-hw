ea9220e0fdeb03cda9f1b5197d59a8551b5fd1df
Vedmax <max.vedernikov@gmail.com>
2015-11-29 22:25:49 +0500

    *баг с NUnit v3.0.0

---
+++ TagCloud/TagCloud/CloudGeneratorShould.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace TagCloud
+{
+	public class CloudGeneratorShould
+	{
+		[Test]
+		public void FindAllWordsInTextFile()
+		{
+			var parser = new TextFileParser();
+			var dict = parser.CreateDictionaryFromFile(@"../../test.txt").Keys.ToArray();
+		}
+	}
+}
