9b628513deb4135b473d33ff1847bcaf53e987f1
Alexandr Promakh <promakh@skbkontur.ru>
2015-11-30 12:45:13 +0500

    feedback

--- TagCloud/TagCloud/Program.cs
+++ TagCloud/TagCloud/Program.cs
@@ -10,12 +10,26 @@ namespace TagCloud
 {
 	class Program
 	{
+        // нужно добавить какой нибудь help для аргументов командной строки, а лучше попробуй поиспользовать
+        // https://github.com/gsscoder/commandline.
+
+        // ну и еще я не вижу реализацию обязательной фичи по указыванию цвета и шрифта на изображении
 		static void Main(string[] args)
 		{
 			var kernel = new Ninject.StandardKernel();
 			kernel.Bind<CommandLineArgs>().ToConstant(new CommandLineArgs(args));
+
+            // вместо kernel.Get<CommandLineArgs>().ConfigFile) нужно использовать фичу,
+            // когда контейнер сам подставляет интерфейсы в аргументы
 			kernel.Bind<Config>().ToConstant(new Config(kernel.Get<CommandLineArgs>().ConfigFile));
 			kernel.Bind<IFileParser>().To<TextFileParser>();
+
+            // проблема в том, что алгоритм генерации облака и способ сохранения зашиты в ConsoleApp
+            // нужно на интерфейсы биндить и алгоритм генерации и способ вывода
+            // например
+            // kernel.Bind<IDrawer>().To<SimpleDrawer>(...); 
+            // kernel.Bind<ISaver>().To<ImageSaver>(...); 
+
 			kernel.Bind<IApplication>().To<ConsoleApp>();
 			kernel.Get<IApplication>().Start();
 		}
--- TagCloud/TagCloud/SimpleDrawer.cs
+++ TagCloud/TagCloud/SimpleDrawer.cs
@@ -18,6 +18,7 @@ namespace TagCloud
 				var sum = words.Take(config.Count).Sum(x => x.Value);
 				foreach (var word in words.Take(config.Count))
 				{
+                    // попробуй порефакторить, например вот эти 3 строчки можно выделить в функцию DrawWord и сразу станет понятнее что происходит 
 					var font = new Font(FontFamily.GenericSansSerif, (bitmap.Height / sum) * word.Value, GraphicsUnit.Pixel);
 					graphics.DrawString(word.Key, font, new SolidBrush(Color.Red), new PointF(0, height));
 					height += (bitmap.Height / sum) * word.Value;
